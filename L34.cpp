b6c4a21dc8e2adb01e8d3ea32ea55570d164994e
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2023-10-15 19:24:53 +0530

    c

--- L34.cpp
+++ L34.cpp
@@ -1,4 +1,4 @@
-/* 34. Find First and Last Position of Element in Sorted Array
+/* 34. Find First and Last Position of Element in Sorted Array → copied [100%]
 Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
 
 If target is not found in the array, return [-1, -1].
@@ -11,12 +11,64 @@ Input: nums = [5,7,7,8,8,10], target = 8
 Output: [3,4]
 */
 
-
-
 #include<iostream>
 #include<vector>
 #include<algorithm>
 using namespace std;
+
+int findFirst(vector<int>& nums, int target) {
+    int left = 0, right = nums.size() - 1;
+    int first = -1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            if (mid == 0 || nums[mid - 1] != target) {
+                first = mid;
+                break;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return first;
+}
+
+int findLast(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        int last = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == nums.size() - 1 || nums[mid + 1] != target) {
+                    last = mid;
+                    break;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return last;
+    }
+
+vector<int> searchRange(vector<int>& nums, int target) {
+    int first = findFirst(nums, target);
+    int last = findLast(nums, target);
+    return {first, last};
+}
 
 void arris(vector<int>& arr){
     cout<<"Give array ele are\n";
@@ -24,44 +76,12 @@ void arris(vector<int>& arr){
         cout<<arr[i]<<" ";
 }
 
-vector<int> searchRange(vector<int>& nums, int target) {
-    int start = 0,end = nums.size()-1;
-    int mid,startIndex = -1,endIndex = -1;
-    cout<<"\nEntered the fun \n";
-    cout<<"Start = "<<start<<" end = "<<end<<"\n";
-    while(start <= end){
-        mid = start+(end-start)/2;
-        if(nums[mid] == target){
-            // cout<<"target = "<<mid; 
-            startIndex = mid;
-            if(nums[startIndex+1] == target)
-                endIndex = startIndex+1;
-            else if(nums[startIndex-1] == target){
-                endIndex = startIndex;
-                startIndex = startIndex - 1;
-            }
-            // cout<<" startindex "<<startIndex<<endl;
-            // cout<<" endindex "<<endIndex<<endl;
-            break;
-        }
-        else if(nums[mid] < target)
-            start = mid + 1;
-        else
-            end = mid - 1;
-    }
-    if(startIndex != -1 && endIndex == -1)
-        endIndex = startIndex;
-    vector<int> resArr = {startIndex,endIndex};
-
-    return resArr;
-}
-
 int main(){
     vector<int> resultarr, arr = {1};
     // vector<int> resultarr, arr = {5,7,7,8,8,10};
     arris(arr);
     resultarr = searchRange(arr,1);
-    cout<<"result array \n";
+    cout<<"\nResult array \n";
     for (int i = 0; i < 2; i++)
         cout<<resultarr[i]<<" ";
 
