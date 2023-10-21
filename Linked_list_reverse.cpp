e7108beee64c698be32ee61446d774663ab15570
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2023-10-21 22:36:02 +0530

    commit

---
+++ Linked_list_reverse.cpp
@@ -0,0 +1,75 @@
+// BASICS of LINKED LIST 
+
+#include <iostream>
+using namespace std;
+
+class node{
+	public: int data;   node* next;
+
+    node(int val){
+    data = val;
+    next = NULL;
+    }	
+};
+
+node* insertAtBeg(node* &head,int val){
+    node* n = new node(val);
+    if(head == NULL)
+        return n;
+    
+    node* temp;
+    n->next = head;
+    return n;
+}
+
+void insertAtTail(node* &head,int val){
+
+
+    node* n = new node(val);
+
+    if(head == NULL){
+        head = n;
+        return;
+    }
+    node* temp = head;
+
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+
+}
+
+void displayll (node* head){
+    node* temp = head;
+    if(temp == NULL)
+        cout<<"There is no node in Linked list\n";
+
+    cout<<"Elements in Linked list are\n";
+    while(temp != NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }cout<<"NULL\n";
+}
+
+int main() {
+    cout<<"\nLinked list\n\n";
+    node* head = new node(1);
+    node* sec = new node(2);
+    head->next = sec;
+    displayll(head);
+
+    cout<<"\nInserting 3 at the END of Linked list\n";
+    insertAtTail(head,3);
+    displayll(head);
+
+    cout<<"\nInserting 0 at the Beginning of Linked list\n";
+    head = insertAtBeg(head,0);
+    displayll(head);
+
+    cout<<"\n\n";
+
+
+
+    return 0;
+}
