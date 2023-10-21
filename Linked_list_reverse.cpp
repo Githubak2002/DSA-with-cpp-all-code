c52a66e8c88a62b2e0d9efde4b146c6e08cc94d7
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2023-10-21 23:45:46 +0530

    commit

--- Linked_list_reverse.cpp
+++ Linked_list_reverse.cpp
@@ -3,35 +3,34 @@
 #include <iostream>
 using namespace std;
 
-class node{
-	public: int data;   node* next;
-
-    node(int val){
-    data = val;
-    next = NULL;
-    }	
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+    // ListNode() : val(0), next(NULL) {}
 
-node* insertAtBeg(node* &head,int val){
-    node* n = new node(val);
+ListNode* insertAtBeg(ListNode* &head,int val){
+    ListNode* n = new ListNode;
+    n->val = val;
     if(head == NULL)
         return n;
     
-    node* temp;
+    ListNode* temp;
     n->next = head;
     return n;
 }
 
-void insertAtTail(node* &head,int val){
-
-
-    node* n = new node(val);
-
+void insertAtTail(ListNode* &head,int val){
+    ListNode* n = new ListNode;
+    n->val = val;
     if(head == NULL){
         head = n;
         return;
     }
-    node* temp = head;
+    ListNode* temp = head;
 
     while(temp->next != NULL){
         temp = temp->next;
@@ -40,36 +39,109 @@ void insertAtTail(node* &head,int val){
 
 }
 
-void displayll (node* head){
-    node* temp = head;
+void displayll (ListNode* head){
+    ListNode* temp = new ListNode;
+    temp = head;
     if(temp == NULL)
-        cout<<"There is no node in Linked list\n";
+        cout<<"There is no ListNode in Linked list\n";
 
     cout<<"Elements in Linked list are\n";
     while(temp != NULL){
-        cout<<temp->data<<"->";
+        cout<<temp->val<<"->";
         temp = temp->next;
     }cout<<"NULL\n";
 }
 
+// not working [ reverse Ll ]
+ListNode* reversell(ListNode* &head){
+    // ListNode* newHead = new ListNode;
+    // newHead =  reversell(head->next);
+
+    ListNode* newHead =  reversell(head->next);
+    head->next->next = head;
+    head->next = NULL;
+
+    return newHead;
+}
+
+// my code not working
+// ListNode* deleteDuplicates(ListNode* head) {
+//     if(head == NULL)
+//         return head;
+//     ListNode* temp = new ListNode;
+//     ListNode* prev = new ListNode;
+//     ListNode* nextNode = new ListNode;
+//     prev = head;
+//     int flag;
+//     while(prev != NULL){
+//         flag = 0;
+//         nextNode = prev->next;
+//         while(prev->val == nextNode->val){
+//             nextNode = nextNode->next;
+//             flag = 1;
+//         }
+//         if(flag == 1){
+//             prev->next = nextNode;
+//         }
+//         else
+//             prev = prev->next;
+
+//     }
+//     return head;      
+// }
+
+
+// GPT code
+ListNode* deleteDuplicates(ListNode* head) {
+    if (head == nullptr || head->next == nullptr) {
+        return head;
+    }
+
+    ListNode* current = head;
+
+    while (current != nullptr && current->next != nullptr) {
+        if (current->val == current->next->val) {
+            ListNode* duplicate = current->next;
+            current->next = duplicate->next;
+            delete duplicate; // Free the memory of the duplicate node
+        } else {
+            current = current->next;
+        }
+    }
+
+    return head;
+}
+
+
 int main() {
-    cout<<"\nLinked list\n\n";
-    node* head = new node(1);
-    node* sec = new node(2);
-    head->next = sec;
+    cout<<"\nLinked list -----------\n\n";
+    ListNode* head = nullptr;
+    head = insertAtBeg(head,3);
+    head = insertAtBeg(head,2);
+    head = insertAtBeg(head,2);
+    head = insertAtBeg(head,1);
+    head = insertAtBeg(head,0);
+    displayll(head);
+
+    /*  cout<<"\nInserting 0 at the Beginning of Linked list\n";
+    head = insertAtBeg(head,0);
     displayll(head);
 
     cout<<"\nInserting 3 at the END of Linked list\n";
     insertAtTail(head,3);
+    displayll(head);    */
+
+    // ListNode* newHead = nullptr;
+    // cout<<"\nReverseing the Linked list\n";
+    // head = reversell(head);
+    // displayll(head);
+
+    cout<<"Deleting duplicate nodes of Ll\n";
+    head = deleteDuplicates(head);
     displayll(head);
 
-    cout<<"\nInserting 0 at the Beginning of Linked list\n";
-    head = insertAtBeg(head,0);
-    displayll(head);
-
-    cout<<"\n\n";
 
 
-
+    cout<<"\n----------------";
     return 0;
 }
