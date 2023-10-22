2f30e55c924e21e4a6cde70dd6320dcaffc9362b
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2023-10-22 23:25:27 +0530

    c

--- a_basic_structure.cpp
+++ a_basic_structure.cpp
@@ -15,3 +15,22 @@ int main(){
     
     return 0;
 }
+
+
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+
+// void printarray(vector<int> arr){
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         cout<<arr[i]<<" ";
+//     }
+    
+// }
+
+// int main(){
+    
+//     return 0;
+// }
