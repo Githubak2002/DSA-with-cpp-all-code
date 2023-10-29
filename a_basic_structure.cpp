f59c43b742809b97e209dd4ec17815fc6d114523
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2023-10-29 22:07:37 +0530

    commit

--- a_basic_structure.cpp
+++ a_basic_structure.cpp
@@ -1,18 +1,19 @@
+// 263. Ugly Number
+
 #include<iostream>
-#include<vector>
 #include<algorithm>
 using namespace std;
 
-void printarray(vector<int> arr){
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cout<<arr[i]<<" ";
-    }
+bool isUgly(int n) {
+    n = abs(n);
+    if(n == 1 || n== 0)
+        return true;
     
 }
 
 int main(){
-    
+    int n = 6;
+    cout<<"is Ugly "<<isUgly(n);
     return 0;
 }
 
