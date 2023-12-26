ea253a105f03cc71fa46c76a6297c1e8375249b3
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2023-12-26 12:12:57 +0530

    commit

--- L896_monotonic_array.cpp
+++ L896_monotonic_array.cpp
@@ -12,19 +12,50 @@ Input: nums = [1,2,2,3]
 Output: true
 */
 
-#include<iostream>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-bool isMonotonic(vector<int>& nums) {
-        
+bool isMonotonic(vector<int> &nums)
+{
+  bool isMonotonic = 0;
+  int f = 0;
+
+  for (int i = 1; i < nums.size(); i++)
+  {
+    if (nums[i] < nums[i - 1])
+    {
+      f = 1;
+      break;
+    }
+  }
+
+  if (f == 0)
+    return 1;
+
+  f = 0;
+
+  for (int i = 1; i < nums.size(); i++)
+  {
+    if (nums[i] > nums[i - 1])
+    {
+      f = 1;
+      return 0;
+    }
+  }
+
+  return 1;
 }
 
-int main(){
-  vector<int> arr = {1,2,2,3};
+int main()
+{
+  vector<int> arr1 = {1, 2, 2, 3};
+  // vector<int> arr = {8,4,4,3};
+  vector<int> arr = {1, 1};
   // int arr[4] = {1,2,2,3};
-  cout<<"\n\n==============";
-  cout<<isMonotonic(arr)<<endl;
+  cout << "\n\n==============\n";
+  cout << endl
+       << isMonotonic(arr) << endl;
   return 0;
 }
