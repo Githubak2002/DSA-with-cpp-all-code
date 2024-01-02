e19866a1538c7a7d18c2b6294265a73eac68b05b
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-01-02 15:15:41 +0530

    commit

--- sort_0_1.cpp
+++ sort_0_1.cpp
@@ -14,13 +14,36 @@ void printArr(vector<int> arr){
   cout<<endl;
 }
 
+void sort01(vector<int> arr){
+  cout<<"sort\n";
+  int i = 0, j = arr.size()-1;
+  while(i<j){
+    while (arr[i]==0)
+      i++;
+    while (arr[j]==1)
+      j--;
+
+    if(i<j){
+      swap(arr[i],arr[j]);
+      i++;
+      j--;
+    }
+    printArr(arr);
+    
+  }
+}
+
+
+
 
 int main(){
   cout<<"\n\n==============\n";
 
-  vector<int> arr = {1,0,0,0,1,0,1};
+  vector<int> arr = {1,1,0,0,1,0,1};
   printArr(arr);
 
-  
+  sort01(arr);
+  printArr(arr);
+
   return 0;
 }
