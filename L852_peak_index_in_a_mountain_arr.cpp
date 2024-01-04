eb77831aff42b1bfedb2896192683418bf97567a
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-01-04 14:20:05 +0530

    commit

--- L852_peak_index_in_a_mountain_arr.cpp
+++ L852_peak_index_in_a_mountain_arr.cpp
@@ -15,7 +15,7 @@ Output: 1
 // binary code
 
 #include<iostream>
-#include<algorithm>
+#include<vector>
 using namespace std;
 
 int binarySearch(int arr[],int n, int key){
@@ -32,13 +32,59 @@ int binarySearch(int arr[],int n, int key){
   return -1;
 }
 
+int peakIndex(int arr[],int n){
+  int start = 0, end = n-1,mid;
+  while(start != end){
+    mid = start + (end-start)/2;
+    // cout<<"\nmid = "<<mid<<endl;
+    if(arr[mid] < arr[mid+1])
+      start = mid + 1;
+    else if(arr[mid]> arr[mid+1])
+      end = mid;
+      // end = mid - 1;
+  }
+  return start;
+}
+
+int peakIndexInMountainArray(vector<int>& arr){
+  int start = 0, end = arr.size()-1,mid;
+  while(start != end){
+    mid = start + (end-start)/2;
+    if(arr[mid] < arr[mid+1])
+      start = mid + 1;
+    else if(arr[mid]> arr[mid+1])
+      end = mid;
+  }
+  return start;
+}
+
+// int peakIndex(int arr[],int n){
+//   int start = 0, end = n-1,mid;
+//   while(start < end){
+//     mid = start + (end-start)/2;
+//     if(arr[mid] < arr[mid+1])
+//       start = mid + 1;
+//     else if(arr[mid]> arr[mid+1])
+//       end = mid - 1;
+//   }
+//   return start;
+// }
+
 int main(){
   cout<<"\n\n==============\n";
 
-  int arr1[6] = {1,3,4,5,6,7};
-  int arr2[7] = {1,2,3,4,5,6,7};
+  vector<int> arr = {4,5,6,5,4};
+  int arr1[6] = {1,3,5,6,2,1};
+  int arr2[7] = {6,7,5,4,3,2,1};
+  int arr3[7] = {1,2,3,5,4,3,1};
+  int arr4[7] = {7,6,5,4,3,2};
 
-  cout<<binarySearch(arr2,7,5);
+  // cout<<binarySearch(arr2,7,5);
+  cout<<"peak index of array is = "<<peakIndex(arr1,6);
+
+  cout<<"peak index of array is = "<<peakIndexInMountainArray(arr);
+
+  
 
   return 0;
 }
