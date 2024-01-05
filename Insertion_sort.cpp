4c99c7aaaa461586e38265735648bdac8066f071
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-01-05 15:19:28 +0530

    commit

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -1,11 +1,34 @@
 // INSERTION SORT
 
 #include<iostream>
-#include<algorithm>
+#include<vector>
+// #include<algorithm>
 using namespace std;
+
+void printarray(vector<int> arr){
+  for (int i = 0; i < arr.size(); i++)
+    cout<<arr[i]<<" "; 
+
+  cout<<endl;
+}
+
+void insertionSort(vector<int> &arr){
+  for (int i = 1; i < arr.size(); i++){
+    for (int j = i; j > 0; j--){
+      if(arr[j-1] > arr[j])
+        swap(arr[j-1],arr[j]);
+    } 
+  }
+  // printarray(arr);
+}
 
 int main(){
   cout<<"\n\n==============\n";
+  vector<int> arr = {3,6,5,2,8,1};
+  printarray(arr);
+  insertionSort(arr);
+  printarray(arr);
+
   return 0;
 }
 
@@ -15,10 +38,3 @@ int main(){
 // #include<algorithm>
 // using namespace std;
 
-void printarray(vector<int> arr){
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    
-}
