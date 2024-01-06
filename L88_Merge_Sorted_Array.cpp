167d4f07a8c5d4ae2a27f37379d7fe7c4315ebd9
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-01-06 16:33:04 +0530

    commit

--- L88_Merge_Sorted_Array.cpp
+++ L88_Merge_Sorted_Array.cpp
@@ -1,2 +1,59 @@
 // 88. Merge Sorted Array
 
+/*
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void printarray(vector<int> arr){
+  for (int i = 0; i < arr.size(); i++)
+    cout<<arr[i]<<" ";    
+  cout<<endl;   
+}
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
+  int x = m;
+  for (int c = 0; c < n; c++){
+    nums1[x] = nums2[c];
+    x++;
+  }
+  printarray(nums1);
+  sort(nums1.begin(),nums1.end());
+  printarray(nums1);
+}
+
+void merge2(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+  vector<int> ans;
+  int i=0,j=0;
+  while( (i+j) != (m+n) ){
+    if(nums1[i]<=nums2[j]){
+      ans.push_back(nums1[i]);
+      i++;
+    }
+    else{ 
+      ans.push_back(nums2[j]);
+      j++;
+    }
+  }
+  nums1 = ans;
+  printarray(nums1);
+  
+}
+
+int main(){
+  cout<<"\n\n==============\n";
+
+  vector<int>arr1 = {1,3,5,0,0,0};
+  vector<int>arr2 = {2,4,6};
+
+  // vector<int> res(arr1);
+  // printarray(res);
+  merge(arr1,3,arr2,3);
+  
+  return 0;
+}
