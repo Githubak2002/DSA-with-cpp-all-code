c0f20fe9f59992504e7633c59809e8f90f7c3298
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-01-07 19:30:58 +0530

    commit

--- L125_Valid_Palindrome.cpp
+++ L125_Valid_Palindrome.cpp
@@ -21,41 +21,35 @@ char lowerCaseFun(char ch){
 }
 
 // fun to convert each Upper case char in string to Lower case
-string lowerCaseFun(string s){
-  int start = 0,end = s.length()-1;
-  for (int i = 0; i <= end; i++)
-  {
-    if(s[i] >= 'A' && s[i] <= 'Z')
-      s[i] = s[i]+32;
-    else {
-      continue;
-    }
+string lowerCaseFun(string s) {
+  for (int i = 0; i < s.length(); i++) {
+      if (s[i] >= 'A' && s[i] <= 'Z') {
+          s[i] = s[i] + 32; // Convert uppercase to lowercase
+      }
   }
   cout<<"string converted "<<s<<endl;
   return s;
 }
 
 // ignorring special char and checking if string is palindron not case sensitive
-bool isPalindrome(string s){
-  s = lowerCaseFun(s);
-  cout<<"Lower case string = "<<s<<endl;
+bool isPalindrome(string s) {
+    s = lowerCaseFun(s);
 
-  int start = 0,end = s.length()-1;
-  while(start<=end){
-    if( (s[start] >= 'a' && s[start] <= 'z' && s[end] >= 'a' && s[end] <= 'z') || (s[start] <= 9 && s[start] >= 0 && s[end] <= 9 && s[end] >= 0) ){
-      cout<<"STRING IS = "<<s<<endl;
-      cout<<"start = "<<start<<" end = "<<end<<endl;
-      if(s[start++] != s[end--]){
-        return false;
-      }
+    int start = 0, end = s.length() - 1;
+    while (start <= end) {
+        if (!(s[start] >= 'a' && s[start] <= 'z') && !(s[start] >= '0' && s[start] <= '9')) {
+            start++;
+        } else if (!(s[end] >= 'a' && s[end] <= 'z') && !(s[end] >= '0' && s[end] <= '9')) {
+            end--;
+        } else {
+            if (s[start++] != s[end--]) {
+                return false;
+            }
+        }
     }
-    else if( !(s[start] >= 'a' && s[start] <= 'z') )
-      start++;
-    else if( !(s[end] >= 'a' && s[end] <= 'z') )
-      end--;
-  }
-  return true;
+    return true;
 }
+
 
 int main(){
   cout<<"\n\n==============\n";
@@ -66,9 +60,9 @@ int main(){
   string s4 = "RaCE is11s;i ECAR";
   string s5 = "0P";
   string empty_s = " ";
-  cout<<"String to lower case = "<<lowerCaseFun(s5)<<endl;
+  cout<<"String to lower case = "<<lowerCaseFun1(s5)<<endl;
   cout<<"String s = "<<s5<<endl;
-  cout<<"Is string palindrom = "<<isPalindrome(s5)<<endl;
+  cout<<"Is string palindrom = "<<isPalindrome1(s5)<<endl;
 
   // cout<<"String length =  "<<s.length()<<endl;
   // cout<<"First=  "<<s[0]<<endl;
