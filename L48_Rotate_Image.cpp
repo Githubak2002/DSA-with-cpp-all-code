7039ae1dc4fb6a9cde0ef687709bf3035f532483
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-01-11 15:44:20 +0530

    rotate image - DSA

--- L48_Rotate_Image.cpp
+++ L48_Rotate_Image.cpp
@@ -50,10 +50,13 @@ int main(){
   cout << "\n\n=======================\n\n";
 
   vector<vector<int>>arr = {{1,2,3},{4,5,6},{7,8,9}};
+  vector<vector<int>>arr2 = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
 
-  print2Darr(arr);
-  rotate(arr);
-  print2Darr(arr);
+  cout<<"2D array"<<endl;
+  print2Darr(arr2);
+  rotate(arr2);
+  cout<<"\n2D array after rotating to 90 deg"<<endl;
+  print2Darr(arr2);
 
 
   cout << "\n\n=======================";
