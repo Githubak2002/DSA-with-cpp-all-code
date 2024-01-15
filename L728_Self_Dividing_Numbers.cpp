2884a9d253f19251a983329e988ef4755419c757
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-01-15 21:29:19 +0530

    DSA

---
+++ L728_Self_Dividing_Numbers.cpp
@@ -0,0 +1,22 @@
+// 728. Self Dividing Numbers
+
+#include<iostream>
+#include<vector>
+// #include<algorithm>
+using namespace std;
+
+int main(){
+  cout << "\n\n=======================\n\n";
+
+  cout << "\n\n=======================";
+  return 0;
+}
+
+// sort(arr.begin(), arr.end());
+
+// print arr ele
+void printarray(vector<int> arr){
+  for (int i = 0; i < arr.size(); i++)
+    cout<<arr[i]<<" ";    
+  cout<<endl;   
+}
