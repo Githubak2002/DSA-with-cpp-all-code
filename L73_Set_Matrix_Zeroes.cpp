70cef5dbe8cebec7c0af0bf67194faa04d68d797
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-02-17 19:09:37 +0530

    DSA - L73. Set Matrix Zeroes

--- L73_Set_Matrix_Zeroes.cpp
+++ L73_Set_Matrix_Zeroes.cpp
@@ -5,10 +5,11 @@
 using namespace std;
 
 // print arr ele
-void printarray(vector<int> arr){
+void printarray(vector<int> arr)
+{
   for (int i = 0; i < arr.size(); i++)
-    cout<<arr[i]<<" ";    
-  cout<<endl;   
+    cout << arr[i] << " ";
+  cout << endl;
 }
 
 // print 2D arr ele
@@ -25,46 +26,46 @@ void print2Darr(vector<vector<int>> arr)
   }
 }
 
-void setZeroes(vector<vector<int>>& matrix) {
+void setZeroes(vector<vector<int>> &matrix)
+{
   int rows = matrix.size(), cols = matrix[0].size();
-  vector<int> tmpr,tmpc;
+  vector<int> tmpr(rows, 1), tmpc(cols, 1);
   for (int row = 0; row < rows; row++)
   {
     for (int col = 0; col < cols; col++)
     {
-      if(matrix[row][col] == 0){
-        tmpr.push_back(row);
-        tmpc.push_back(col);
-
-        // cout<<"i = "<<row<<" j = "<<col<<endl;
-        // tmpc = col; tmpr = row;
-        // for (int row = 0; row < rows; row++)
-        //   matrix[row][tmpc] = 0;
-        // for (int col = 0; col < cols; col++)
-        //   matrix[tmpr][col] = 0;
+      if (matrix[row][col] == 0)
+      {
+        tmpr[row] = 0;
+        tmpc[col] = 0;
       }
     }
   }
-
-  for (int k = 0; k < cols; k++)
-  {
-    
-  }
-  
   // printarray(tmpr);
   // printarray(tmpc);
+  for (int i = 0; i < tmpr.size(); i++)
+  {
+    for (int j = 0; j < tmpc.size(); j++)
+    {
+      if (tmpr[i] == 1 && tmpc[j] == 1)
+        continue;
+      else
+        matrix[i][j] = 0;
+    }
+  }
 }
 
 int main()
 {
   cout << "\n\n=======================\n\n";
 
-  vector<vector<int>> arr = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+  vector<vector<int>> arr1 = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+  vector<vector<int>> arr = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1,3,1,5}};
 
   print2Darr(arr);
   setZeroes(arr);
-  // cout<<"After setting zeros\n";
-  // print2Darr(arr);
+  cout << "After setting zeros\n";
+  print2Darr(arr);
 
   cout << "\n\n=======================";
   return 0;
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,149 @@
-n
+// L141_Linked_List_Cycle.cpp
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class ListNode
+{
+public:
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void printLL(ListNode *head)
+{
+  if(head == NULL){
+    cout<<"LL is empty"<<endl;
+    return;
+  }
+  // cout<<"aaya";
+  int h = -4;
+  ListNode *temp;
+  temp = head;
+  while (temp->val != h)
+  {
+    cout << temp->val << "->";
+    temp = temp->next;
+  }
+  cout << temp->val <<endl;
+      //  << "NULL" << endl;
+}
+
+void addAtEnd(ListNode *&head, int newData)
+{
+  ListNode *newNode = new ListNode(newData);
+  if (head == nullptr)
+  {
+    head = newNode;
+    return;
+  }
+  ListNode *current = head;
+  while (current->next != nullptr)
+  {
+    current = current->next;
+  }
+  current->next = newNode;
+}
+
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+{
+  ListNode *result = nullptr;
+  int sum=0,rem=0,carry = 0;
+  while(l1 != NULL && l2 != NULL ){
+    sum = 0;  rem = 0;
+    sum = l1->val + l2->val + carry;
+    if(sum%10 != sum){
+      rem = sum%10;
+      carry = 1;
+      addAtEnd(result,rem);
+    }
+    else{
+      carry = 0;
+      addAtEnd(result,sum);
+    }
+    l1 = l1->next;
+    l2 = l2->next;
+  }
+
+  while(l1 != NULL){
+    // if(carry == 1)
+    sum = carry+l1->val;
+
+    if(sum%10 != sum){
+      rem = sum%10;
+      carry = 1;
+      addAtEnd(result,rem);
+    }
+    else{
+      carry = 0;
+      addAtEnd(result,sum);
+    }
+    l1 = l1->next;
+  }
+
+  while(l2 != NULL){
+    // if(carry == 1)
+    sum = carry+l2->val;
+
+    if(sum%10 != sum){
+      rem = sum%10;
+      carry = 1;
+      addAtEnd(result,rem);
+    }
+    else{
+      carry = 0;
+      addAtEnd(result,sum);
+    }
+    l2 = l2->next;
+  }
+
+  if(carry == 1)
+    addAtEnd(result,1);
+
+  return result;
+}
+
+bool hasCycle(ListNode *head){
+  
+}
+
+int main()
+{
+  cout << "\n\n";
+
+  // ListNode *head = new ListNode;
+  ListNode *n0 = new ListNode;
+  ListNode *n1 = new ListNode;
+  ListNode *n2 = new ListNode;
+  ListNode *n3 = new ListNode;
+
+  n0->val = 3;  n0->next = n1;
+  n1->val = 2;  n1->next = n2;
+  n2->val = 0;  n2->next = n3;
+  n3->val = -4;  n3->next = n1;
+
+  printLL(n0);
+
+  cout<<"is there cycle in LL"<<hasCycle(n0)<<endl;
+
+  /*
+  addAtEnd(n1,9);
+  addAtEnd(n1,9);
+  addAtEnd(n1,9);
+  addAtEnd(n1,9);
+
+  addAtEnd(n2,9);
+  addAtEnd(n2,9);
+  addAtEnd(n2,9);
+  addAtEnd(n2,9);
+  addAtEnd(n2,9);
+  addAtEnd(n2,9);
+  */
+
+  cout << "\n\n";
+  return 0;
+}
