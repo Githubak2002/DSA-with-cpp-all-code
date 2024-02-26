ffa20c80685915da6c98de9229cc42ff5d3b7dfa
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-02-26 19:50:02 +0530

    DSA - Recursion

--- Recursion/String_palindrom.cpp
+++ Recursion/String_palindrom.cpp
@@ -10,29 +10,37 @@ void printarray(vector<int> arr)
   cout << endl;
 }
 
-
 // REVERSE AN ARRAY
-void reverseArr(int arr[],int start,int end){
-  if(start >= end)
+void reverseArr(int arr[], int start, int end)
+{
+  if (start >= end)
     return;
 
-  swap(arr[start],arr[end]);
-  reverseArr(arr,start+1,end-1);
+  swap(arr[start], arr[end]);
+  reverseArr(arr, start + 1, end - 1);
 }
 
-// CHECK IF STRING IS PALINDROM 
-bool isPalindrom(string s,int len){
+// CHECK IF STRING IS PALINDROM
+bool isPalindrom(string s, int i)
+{
+  if (i >= s.length() / 2)
+    return true;
+
+  if (s[s.length() - i - 1] != s[i])
+    return false;
+  return isPalindrom(s, ++i);
+  // isPalindrom(s, ++i);
 
 }
 
 int main()
 {
-  cout<<"\n\n";
+  cout << "\n\n";
 
   string s = "abcdcba";
   int len = s.length();
-  cout<<"is "<<s<<" palindrom = "<<isPalindrom(s)<<endl;
+  cout << "is " << s << " palindrom = " << isPalindrom(s, 0) << endl;
 
-  cout<<"\n\n";
+  cout << "\n\n";
   return 0;
 }
